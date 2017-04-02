a36ef0f99c612310188bde72fbc9fe4e02ebdee5
giulioventuri <giulio.venturi@gmail.com>
2017-04-02 10:47:18 +0200

    giovedì 2 marzo: Tester Batteria

--- testbatt.ino
+++ testbatt.ino
@@ -1,5 +1,20 @@
 /* Urbania, 2 marzo 2017
    corso base Arduino
    esempio tester batteria
+   
+   Arduino legge il pin A1.
+   La lettura e' un numero da 0 a 1023 ( campionamento a 10 bit )
+   Il calcolo 5.0*a/1023 risolve la proporzione e restituisce i volt
+   (funziona solo con differenze di potenziale positive e minori di 5 volt!!)
 */
+void setup() {
+  pinMode(A1,INPUT);
+  Serial.begin(115200);
+}
 
+int a = 0;
+void loop() {
+  // put your main code here, to run repeatedly:
+  a=analogRead(A1);
+  Serial.println(5.0*a/1023,3);
+}
